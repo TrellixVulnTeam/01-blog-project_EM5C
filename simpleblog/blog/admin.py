301d62e01fe28737c88d197816e9aefb63671302
Juliano Souza <juliano.souza@aeb.gov.br>
2022-05-30 11:39:24 -0300

    first commit

---
+++ simpleblog/blog/admin.py
@@ -0,0 +1,10 @@
+# Register your models here.
+from django.contrib import admin
+
+from .models import Post
+
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "created", "updated")
+    prepopulated_fields = {"slug": ("title",)}
---
+++ simpleblog/blog/views.py
@@ -0,0 +1,10 @@
+import imp
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
